8696d7e8896d4913e2bd4808bf79479e841068b2
Nobuhiro Saijo <saijo.magic@gmail.com>
2018-03-13 21:13:56 +0900

    fix:principle

--- TraceTarget.cs
+++ TraceTarget.cs
@@ -6,15 +6,26 @@ How to use
 description
 	TargetとなるGameObjectに、位置、姿勢、共に追従させるためのscript.
 	
-		private Vector3 pos;
-		private Vector3 forward;
-	は、それぞれ、GO_TargetのLocal座標系でのposとforward vectorを保持する.
-		InverseTransformPoint()
-		InverseTransformDirection()
-	は、それぞれ、Global -> Localの変換関数.
-	
-	Update()では、posとforwardに保持されたGO_TargetのLocal座標系値を
-	Global値に戻し、transformにset.
+	pos
+		TargetのLocal座標系で、同位置に来るよう処理。
+		
+	rot
+		transform.rotationは、現在いる座標系でのMartix処理(つまりAction)であり、処理結果としての"姿勢"ではない。
+		transform.forward/ up/ right が結果。
+		
+		故に、forwardについて、TargetのLocal座標系で同方向に来るよう処理。
+		さて、これでforward方向は保持されるが、forward vector回りの回転はFixされていないので、姿勢が保持されない。
+		(実際、forwardを法線ベクトルに持つ平面がクルクルと回転してしまう)
+		
+		そこで、up or rightについても、
+			transform.forward	= GO_Target.transform.TransformDirection(TargetLocal_forward);
+			transform.up		= GO_Target.transform.TransformDirection(TargetLocal_up);
+		とすれば良いのかと思ったが、この処理だと、一番最後の処理のみを行ったのと同じになってしまうようだ。
+		つまり、"transform.forward = ..."と処理した部分は、保持されない。
+		
+		そこで、まずはforwardが保持されるように処理を行った上で、
+		予め保存されたup vectorと、forwardを揃えた後でのup vectorの成す角を算出し、
+		これを打ち消すように"RotateAround"することで姿勢の保持をすることとした。
 
 tips
 	Mocapの場合、"PartsであるBoneそのもの"をtargetとして指定すること.
@@ -33,25 +44,51 @@ public class TraceTarget : MonoBehaviour {
 	****************************************/
 	public GameObject GO_Target;
 	
-	private Vector3 pos;
-	private Vector3 forward;
+	private Vector3 TargetLocal_pos;
+	private Vector3 TargetLocal_forward;
+	private Vector3 TargetLocal_up;
 	
+	string label;
 	
 	/****************************************
 	****************************************/
 	void Start () {
-		pos = GO_Target.transform.InverseTransformPoint(transform.position);
-		forward = GO_Target.transform.InverseTransformDirection(transform.forward);
+		TargetLocal_pos		= GO_Target.transform.InverseTransformPoint(transform.position);
+		TargetLocal_forward = GO_Target.transform.InverseTransformDirection(transform.forward);
+		TargetLocal_up		= GO_Target.transform.InverseTransformDirection(transform.up);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position	= Vector3.Lerp(transform.position, GO_Target.transform.TransformPoint(pos), Time.deltaTime * 5.0f/* 200msで追いつくSpeed  */);
-		transform.forward	= Vector3.Lerp(transform.forward, GO_Target.transform.TransformDirection(forward), Time.deltaTime * 5.0f);
+		/********************
+		********************/
+		transform.position	= Vector3.Lerp(transform.position, GO_Target.transform.TransformPoint(TargetLocal_pos), Time.deltaTime * 5.0f/* 200msで追いつくSpeed  */);
+		transform.forward	= Vector3.Lerp(transform.forward, GO_Target.transform.TransformDirection(TargetLocal_forward), Time.deltaTime * 5.0f);
 		
+		float angle = Vector3.Angle(GO_Target.transform.InverseTransformDirection(transform.up), TargetLocal_up);
+		transform.RotateAround(transform.position, transform.forward, -angle);
+		
+		/********************
+		********************/
 		/*
-		transform.position	= GO_Target.transform.TransformPoint(pos);
-		transform.forward	= GO_Target.transform.TransformDirection(relative);
+		transform.position	= GO_Target.transform.TransformPoint(TargetLocal_pos);
+		
+		transform.forward	= GO_Target.transform.TransformDirection(TargetLocal_forward);
+		// transform.up		= GO_Target.transform.TransformDirection(TargetLocal_up); // これだと、上の行(forward)で行われた内容が上書きされてしまう.
+		
+		
+		float angle = Vector3.Angle(GO_Target.transform.InverseTransformDirection(transform.up), TargetLocal_up);
+		transform.RotateAround(transform.position, transform.forward, -angle);
 		*/
+		/*
+		label =		string.Format("{0:0.000000} ,"	, angle);
+		label +=	string.Format("{0:0.000000} ,"	, Vector3.Angle(transform.up, GO_Target.transform.TransformDirection(TargetLocal_up))); // 0 になっているか、check
+		*/
+	}
+	
+	void OnGUI()
+	{
+		GUI.color = Color.red;
+		// GUI.Label(new Rect(15, 30, 500, 30), label);
 	}
 }
